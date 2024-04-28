957cf313529a80331f2dc5496a62ef1ff459a566
javiera1709 <javiemarin@alumnos.uai.cl>
2024-04-28 19:19:04 -0400

    arreglo navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,16 +7,24 @@ function Home() {
 
   return (
     <>
-      <Navbar bg="warning" expand="lg" className="mx-3 my-3 rounded">
+      <style>
+        {`
+          .navbar-link:hover {
+            background-color: #C2FAFB; /* Color de fondo al pasar el mouse sobre los enlaces */
+            border-radius: 10px; /* Agregando el borderRadius al pasar el mouse */
+          }
+        `}
+      </style>
+      <Navbar bg="info" expand="lg" className="mx-3 my-3 rounded">
         <Container fluid>
-          <Navbar.Brand href="/home">{t('welcome')}</Navbar.Brand>
+          <Navbar.Brand className='ml-3' href="/home" style={{ fontSize: '1.2rem' }}>{t('welcome')}</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="w-100 justify-content-evenly">
-              <Nav.Link href="/Home">{t('home')}</Nav.Link>
-              <Nav.Link href="/correspondencia">{t('delivery')}</Nav.Link>
-              <Nav.Link href="/visitas">{t('visitors')}</Nav.Link>
-              <Nav.Link href="/vehiculos">{t('vehicles')}</Nav.Link>
+              <Nav.Link href="/Home" className="navbar-link">{t('home')}</Nav.Link>
+              <Nav.Link href="/correspondencia" className="navbar-link">{t('delivery')}</Nav.Link>
+              <Nav.Link href="/visitas" className="navbar-link">{t('visitors')}</Nav.Link>
+              <Nav.Link href="/vehiculos" className="navbar-link">{t('vehicles')}</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
